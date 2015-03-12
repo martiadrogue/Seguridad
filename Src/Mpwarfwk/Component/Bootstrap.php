e600b89eb386255248cc4e703ed23b2541256cf7
xavier pera <xavier.pera@gmail.com>
2015-03-12 18:38:47 +0100

    cambios composer.json

--- Src/Mpwarfwk/Component/Bootstrap.php
+++ Src/Mpwarfwk/Component/Bootstrap.php
@@ -1,12 +1,21 @@
 <?php 
 
-namespace Mpwarfwk\Component;
+namespace src\Mpwarfwk\Component;
+//use \Src\Controllers;
 
 class Bootstrap {
     
     public function __construct() {
         echo "<h2>Hello World</h2>";
     }
-            
 
+public function execute(){
+
+	$request = Request::returnRequest();
+	$routing = new Routing();
+	$controlerName = $routing->handle($request);
+	echo $controlerName;
+	$controler = new $controlerName();
+	$controller->build();
+	}
 }
