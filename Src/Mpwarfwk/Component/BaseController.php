310683bdf2f272aed0bea83332dd879917407e35
xavier pera <xavier.pera@gmail.com>
2015-03-14 13:54:40 +0100

    Creada clase abstracta BaseController, que servirá de base para nuestros controladores

---
+++ Src/Mpwarfwk/Component/BaseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mpwarfwk\Component;
+
+abstract class BaseController {
+    
+    public function __construct() {
+        echo "Estoy en la clase abstracta BaseController - ";
+    }
+        
+}
