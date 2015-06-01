ab6933c027f94aa259cc792309b42939bb98c995
marti adrogue <marti.adrogue@gmail.com>
2015-06-01 12:08:35 +0200

    Update PdoDatabase::class.

--- src/Mpwarfwk/Database/PdoDatabase.php
+++ src/Mpwarfwk/Database/PdoDatabase.php
@@ -1,18 +1,11 @@
 <?php
-
 namespace Mpwarfwk\Database;
-
 use PDO;
-
 class PdoDatabase {
-
 	private $database;
 	private $dbConnection;
-
 	public function __construct() {
-
 		$this->dbConnection = require('../src/Config/DatabaseConfig.php');
-
 		$db = $this->dbConnection["DATABASE"];
 		$host = $this->dbConnection["HOST"];
 		$user = $this->dbConnection["USER"];
@@ -22,19 +15,14 @@ class PdoDatabase {
 	    $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	    $this->database->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 	}
-
 	public function selectAllFromTable($table) {
-
 		$statement = $this->database->prepare("SELECT * FROM $table");
 		$statement->execute();
 		return $statement->fetchAll();
 	}
-
 	public function selectFromTable($query, $data = NULL) {
-
 		//ESPERA RECIBIR QUERIES CON ESTA ESTRUCTURA => 
 		//"SELECT email FROM subscribers" o "SELECT email FROM subscribers WHERE id = :id"
-
 		$statement = $this->database->prepare($query);
 		if($data != NULL){
 			foreach ($data as $key => $actualValue) {
@@ -44,10 +32,7 @@ class PdoDatabase {
 		$statement->execute();
 		return $statement->fetchAll();
 	}
-
-
 	public function insertInTable($query, $data) {
-
 		//ESPERA RECIBIR QUERIES CON ESTA ESTRUCTURA => "INSERT INTO subscribers SET email = :email"
 		$statement = $this->database->prepare($query);
 		foreach ($data as $key => $actualValue) {
@@ -55,13 +40,14 @@ class PdoDatabase {
 		}
 		return $statement->execute();
 	}
-
-	public function deleteFromTable($table, $id) {
-
-		$statement = $this->database->prepare("DELETE FROM $table WHERE id = '$id' LIMIT 1");
+	public function deleteFromTable($query, $data) {
+		//ESPERA RECIBIR QUERIES CON ESTA ESTRUCTURA => "DELETE FROM $table WHERE id = :id LIMIT 1"
+		$statement = $this->database->prepare($query);
+		foreach ($data as $key => $actualValue) {
+			$statement->bindValue(":$key", $actualValue);
+		}
 		return $statement->execute();
 	}
-
 	public function updateTable($query, $data) {
 		
 		//ESPERA RECIBIR QUERIES CON ESTA ESTRUCTURA => "UPDATE subscribers SET email = :email WHERE id = :id" 
